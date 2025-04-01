7aa3bd367c5c95dde7de969c4280dec9bb7dad8a
YiJun Zou <zapboltisepic@gmail.com>
2025-04-01 16:37:48 +1100

    added frontend...

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@chakra-ui/react"
+import './App.css';
+import './index.css'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Button colorPalette="orange"><div className="text-red-500">HELLO</div></Button>
+    </>
+  )
+}
+
+export default App
